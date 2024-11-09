f567d793bea1e5e0165bde894aefb106e55f57a9
Ares <53051330+Areees777@users.noreply.github.com>
2024-11-09 10:32:30 +0100

    Update

--- app/spark/spark_script.py
+++ app/spark/spark_script.py
@@ -3,7 +3,6 @@ from pyspark.sql import SparkSession
 def main():
     spark = SparkSession.builder \
         .appName("Example Spark Job") \
-        .master("spark://spark_master:7077") \
         .getOrCreate()
 
 if __name__ == "__main__":
