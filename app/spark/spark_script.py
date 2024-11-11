85150b6adc967b6cc3430dd726d0e9f0c70924ef
Ares <53051330+Areees777@users.noreply.github.com>
2024-11-11 18:25:26 +0100

    Update

--- app/spark/spark_script.py
+++ app/spark/spark_script.py
@@ -163,7 +163,7 @@ def main():
     spark = SparkSession.builder \
         .appName("Input data") \
         .config("spark.hadoop.fs.defaultFS", HDFS_URL) \
-        .config("spark.jars", "/opt/bitnami/spark/jars/spark-sql-kafka-0-10_2.12-3.3.0.jar") \
+        .config("spark.jars", "spark-sql-kafka-0-10_2.12-3.3.0.jar") \
         .getOrCreate()
 
     run(spark)    
