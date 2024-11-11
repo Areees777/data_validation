abaa02ad893e1b5f753fa2d7853405bb15ba1d65
Ares <53051330+Areees777@users.noreply.github.com>
2024-11-11 14:33:55 +0100

    Update

--- app/spark/spark_script.py
+++ app/spark/spark_script.py
@@ -145,7 +145,7 @@ def run(spark: SparkSession) -> None:
 def main():
     spark = SparkSession.builder \
         .appName("Input data") \
-        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1") \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
         .config("spark.jars.packages", "org.apache.kafka:kafka-clients:3.9.0") \
         .config("spark.hadoop.fs.defaultFS", HDFS_URL) \
         .getOrCreate()    
