9dd43bd676c626107520169f2f9735eff12efd10
Ares <53051330+Areees777@users.noreply.github.com>
2024-11-11 15:27:05 +0100

    Update

--- app/spark/spark_script.py
+++ app/spark/spark_script.py
@@ -144,9 +144,14 @@ def run(spark: SparkSession) -> None:
 
 def main():
 
+    # packages = [
+    #     "https://repo1.maven.org/maven2/org/apache/kafka/kafka-clients/3.9.0/kafka-clients-3.9.0.jar",
+    #     "https://repo1.maven.org/maven2/org/apache/spark/spark-sql-kafka-0-10_2.12/3.5.0/spark-sql-kafka-0-10_2.12-3.5.0.jar"
+    # ]
+
     packages = [
-        "https://repo1.maven.org/maven2/org/apache/kafka/kafka-clients/3.9.0/kafka-clients-3.9.0.jar",
-        "https://repo1.maven.org/maven2/org/apache/spark/spark-sql-kafka-0-10_2.12/3.5.0/spark-sql-kafka-0-10_2.12-3.5.0.jar"
+        "/opt/bitnami/spark/jars/kafka-clients-3.9.0.jar",
+        "/opt/bitnami/spark/jars/spark-sql-kafka-0-10_2.12-3.5.0.jar"
     ]
 
     # spark = SparkSession.builder \
