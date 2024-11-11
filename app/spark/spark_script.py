fd3db78d5ca46d59d53258218bd996a6474d59bc
Ares <53051330+Areees777@users.noreply.github.com>
2024-11-11 10:30:20 +0100

    Update

--- app/spark/spark_script.py
+++ app/spark/spark_script.py
@@ -1,11 +1,13 @@
 from pyspark.sql import SparkSession
 
 def main():
+    hdfs_url = "hdfs://hadoop:9000"
     spark = SparkSession.builder \
         .appName("Input data") \
+        .config("spark.hadoop.fs.defaultFS", hdfs_url) \
         .getOrCreate()
 
-    file_path = "http://hadoop:50070/data/input.json"
+    file_path = "hdfs://hadoop:9000/data/input.json"
     df = spark.read.parquet(file_path)
     print(f"Count = {df.count()}")
 
