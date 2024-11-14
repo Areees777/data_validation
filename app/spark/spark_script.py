adbc589cc1b9723170f5484e56b5b54505abf1c9
Ares <53051330+Areees777@users.noreply.github.com>
2024-11-14 18:55:15 +0100

    Update

--- app/spark/spark_script.py
+++ app/spark/spark_script.py
@@ -250,27 +250,24 @@ def run(spark: SparkSession) -> None:
 def main():
     spark = SparkSession.builder \
         .appName("airflow_spark_job") \
-        .config("spark.dynamicAllocation.enabled", "false") \
-        .config("spark.executor.memory", "2g") \
-        .config("spark.executor.cores", "1") \
         .config("spark.hadoop.fs.defaultFS", HDFS_URL) \
         .getOrCreate()
 
-    data = [("Alice", "1"), ("Bob", "2"), ("Charlie", "3")]
-    columns = ["name", "value"]
+    # data = [("Alice", "1"), ("Bob", "2"), ("Charlie", "3")]
+    # columns = ["name", "value"]
 
-    df = spark.createDataFrame(data, columns)
+    # df = spark.createDataFrame(data, columns)
 
-    KAFKA_URL = "kafka:9092"
-    TOPIC = "person"
+    # KAFKA_URL = "kafka:9092"
+    # TOPIC = "person"
 
-    df.write \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", KAFKA_URL) \
-        .option("topic", TOPIC) \
-        .save()
+    # df.write \
+    #     .format("kafka") \
+    #     .option("kafka.bootstrap.servers", KAFKA_URL) \
+    #     .option("topic", TOPIC) \
+    #     .save()
 
-    # run(spark)
+    run(spark)
 
 
 if __name__ == "__main__":
