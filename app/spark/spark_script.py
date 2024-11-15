05ff571b27bfeab12726c1ad5305b7283e6e6306
Ares <53051330+Areees777@users.noreply.github.com>
2024-11-15 13:27:30 +0100

    Update

--- app/spark/spark_script.py
+++ app/spark/spark_script.py
@@ -5,6 +5,7 @@ from pyspark.sql import SparkSession, DataFrame, functions as F
 from pyspark.sql.types import StringType
 
 os.environ["PYSPARK_PYTHON"]="/usr/bin/python3"
+os.environ["PYSPARK_DRIVER_PYTHON"]="/usr/bin/python3"
 
 HDFS_URL = "hdfs://hadoop:9000"
 KAFKA_URL = "kafka:9092"
